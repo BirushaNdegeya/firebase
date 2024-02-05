f707c5ce4872142931b4abe452eaa26a6d391731
Birusha Ndegeya <142086785+BirushaNdegeya@users.noreply.github.com>
2024-02-05 14:38:35 +0200

    hello

---
+++ src/pages/Login.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+// import firebase from 'firebase/compat/app';
+// import firebaseui from 'firebaseui';
+// import 'firebaseui/dist/firebaseui.css';
+
+const Login = () => {
+   return (
+      <h1>Loading... Login</h1>
+   );
+};
+
+export default Login;
---
+++ src/pages/SignUp.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const SignUp = () => {
+   return (
+      <h1>Loading... Sign Up</h1>
+   );
+};
+
+export default SignUp;
