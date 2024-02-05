8a37f24f8131c7ffa9b4ff2ea64f0fbe0b6fc114
Birusha Ndegeya <142086785+BirushaNdegeya@users.noreply.github.com>
2024-02-05 13:39:54 +0200

    'New

---
+++ src/config.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDMx5hfeHQmcoRKZWxcI84_Z_wDN7UgqTQ",
+  authDomain: "fir-learning-89320.firebaseapp.com",
+  projectId: "fir-learning-89320",
+  storageBucket: "fir-learning-89320.appspot.com",
+  messagingSenderId: "60427505678",
+  appId: "1:60427505678:web:d2e9508199922ab3a9ac5f"
+};
+
+
+export const app = initializeApp(firebaseConfig);
